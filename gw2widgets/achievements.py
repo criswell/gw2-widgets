4e27ddd4e1aac49b4ab5661557af91647ff2630a
Sam Hart <criswellious@gmail.com>
2015-11-16 11:30:28 -0500

    start to flesh out needed methods

--- gw2widgets/achievements.py
+++ gw2widgets/achievements.py
@@ -13,3 +13,23 @@ class Achievements:
         self.api = GW2_API(config)
         self.ds = DataStore(config)
         self.render = Render(config)
+
+    def _get_current(self):
+        """Get the current achievements for the character."""
+
+        cheeves = self.api.get("account/achievements")
+        cheeves_by_id = {}
+        for cheeve in cheeves:
+            cheeves_by_id[cheeve.id] = cheeve
+
+        return cheeves_by_id
+
+    def _get_new_cheeves(self, old_cheeves, new_cheeves):
+        """Given a dict of old and new Achievements, find those that are
+        newly completed."""
+
+        unlocks = []
+        for cheeve in new_cheeves:
+            if cheeve.done != old_cheeves[cheeve.id].done:
+                unlocks.append(cheeve)
+        return unlocks
